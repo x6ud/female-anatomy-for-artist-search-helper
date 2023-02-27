2b93a617c274a89009f42d07761c30d5e5f7413f
x6ud <x6ud@foxmail.com>
2023-02-27 17:29:46 +0800

    Added more data

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -1,14 +1,12 @@
 import {loadImage} from './image';
 
 export async function proxyGetText(url: string) {
-    const res = await fetch(url);
-    // const res = await fetch('./proxy/' + url);
+    const res = await fetch('./proxy/' + url);
     return await res.text();
 }
 
 export async function proxyGetImage(url: string) {
-    const res = await fetch(url);
-    // const res = await fetch('./proxy-arraybuffer/' + url);
+    const res = await fetch('./proxy-arraybuffer/' + url);
     const blob = await res.blob();
     const objUrl = URL.createObjectURL(blob);
     return await loadImage(objUrl);
